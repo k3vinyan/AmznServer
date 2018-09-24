665ad6dfea2cb9983c880cb64dfef1498a88c0a1
Kevin Yan <k3vinyan@gmail.com>
2018-09-24 00:19:06 -0700

    create misses routes

---
+++ routes/misses.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Miss = require('../models/miss');
+
+router.get('/', (req, res, next) => {
+   Scan.find({})
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.sendStatus(500).json({
+        error: err
+      })
+    })
+})
+
+router.post('/', (req, res, next))
+
+router.post('/', (req, res, next) => {
+  const route = req.body.route;
+
+
+  Scan.estimatedDocumentCount({}, function(err, c){
+    return c;
+  })
+  .then(count => {
+    const scan = new Scan({
+      _id: new mongoose.Types.ObjectId(),
+      tba: req.body.tba,
+      route: req.body.route,
+      cluster: cluster,
+      entry: count
+    })
+
+    scan.save()
+      .then(data => {
+        res.send("data: " + data)
+      })
+      .catch(err => {
+        res.sendStatus(500).json({
+          error: err
+        })
+      })
+  })
+});
+
+router.get('/deleteAll', () =>{
+  Scan.deleteMany()
+    .save()
+    .then( ()=>{
+      res.send("db deleted")
+    })
+    .catch(err => {
+      res.sendStatus(500).json({
+        error: err
+      })
+    })
+});
+
+module.exports = router;
